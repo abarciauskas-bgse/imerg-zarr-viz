8615ed98f5f71d5acfa474f8b285cc3b00a849eb
Aimee Barciauskas <aimee@developmentseed.org>
2022-06-18 12:36:58 -0700

    Update imerg zarr demo

--- pages/index.js
+++ pages/index.js
@@ -28,17 +28,8 @@ const Index = () => {
           display={true}
           opacity={1}
           mode={'texture'}
-          source={'http://localhost:8000/gpmimerg.zarr'}
-          variable={'precipitation'}
-        />
-        <Raster
-          colormap={colormap}
-          clim={[0, 1]}
-          display={displayCog}
-          opacity={1}
-          mode={'texture'}
-          type={'cog'}
-          source={ 'https://ds-data-projects.s3.amazonaws.com/smce-eis/3B-MO.MS.MRG.3IMERG.20200501-S000000-E235959.05.V06B.HDF5.tif' }
+          selector={{ time }}
+          source={'http://localhost:8000/gpmimerg-monthly.zarr'}
           variable={'precipitation'}
         />
         <RegionControls
